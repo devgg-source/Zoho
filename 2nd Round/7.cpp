29b503d54588be44c5cae8a5c438156a91423253
Karthik G G <61579922+devgg-source@users.noreply.github.com>
2020-06-26 20:21:54 +0530

    Add files via upload

---
+++ 2nd Round/7.cpp
@@ -0,0 +1,41 @@
+/*
+1.	Print the word with odd letters as
+
+P         M
+ R      A
+   O  R
+     G
+  O    R
+ R       A
+P          M 
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++)
+    {
+        for (int j = 0; j < s.length(); j++)
+        {
+            if (i == j)
+            {
+                cout << s[i];
+            }
+            else if (j == s.length() - i - 1)
+            {
+                cout << s[j];
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
