50ec0a27cdb2294a8274f21ad298d6e9efab093c
Karthik G G <61579922+devgg-source@users.noreply.github.com>
2020-08-07 16:36:15 +0530

    zoho-4

--- 2nd Round/4.cpp
+++ 2nd Round/4.cpp
@@ -1,12 +1,15 @@
 /*
+
 A number is called as binary-decimal if all the digits in the number should be either ‘1’ or ‘0’. 
 Any number can be written as a sum of binary-decimals. 
 Our task is to find the minimum number of binary-decimals to represent a number.
+
 Input : 32
 Output : 10 11 11
 
 Input : 120
 Output : 10 110
+
 */
 
 #include <iostream>
