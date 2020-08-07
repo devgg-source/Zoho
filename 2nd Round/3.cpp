ab1da130bf63b69a5c237b5fa2713c9960435871
Karthik G G <61579922+devgg-source@users.noreply.github.com>
2020-08-07 16:34:51 +0530

    zoho-3

--- 2nd Round/3.cpp
+++ 2nd Round/3.cpp
@@ -1,8 +1,10 @@
 /*
+
 You’re given a number n. 
 If write all the numbers from 1 to n in a paper, we have to find the number of characters written on the paper.
 For example if n=13, the output should be 17
 if n = 101, the output should be 195
+
 */
 #include <iostream>
 using namespace std;
